3bce869c41c5285d77b384dceb71c32b5aa66425
Jacky Zhou <54785891+yz2001zzx@users.noreply.github.com>
2023-05-07 20:12:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/*', (req, res) => {
-  res.send('Hello Jason Bro');
+  res.send('Hello Jacky, Jason and Vico');
 });
 
 app.listen(PORT, HOST);
